6496a6040b0fefd464ed1506f9c4f7efa3f4ec33
Xun Zhu <zhuxun2@gmail.com>
2016-07-05 17:57:56 -1000

    commit

---
+++ inst/3js/lib/mousestate.js
@@ -0,0 +1,49 @@
+// Usage:
+// new LIB.MouseState   -->  init()
+// mousestate.update()  -->  render()
+//
+var LIB = LIB || {};
+
+LIB.MouseState = function (domElement, camera, objs) {
+  this.domElement= domElement || document;
+  this.camera = camera;
+  this.objs = objs;
+  this.mouse = new THREE.Vector2(Infinity, Infinity, Infinity);
+  this.raycaster = new THREE.Raycaster();
+  this.intersectObj = null;
+  
+  // create callback to bind/unbind keyboard events
+  var _this = this;
+  this._onMouseMove = function(event){ _this._onMouseChange(event)  }
+
+  // bind keyEvents
+  this.domElement.addEventListener("mousemove", this._onMouseMove, false);
+}
+
+LIB.MouseState.prototype.destroy = function() {
+  this.domElement.removeEventListener("mousemove", this._onMouseMove, false);
+};
+
+LIB.MouseState.prototype._onMouseChange = function(event) {
+  this.mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+  this.mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+};
+
+LIB.MouseState.prototype.update = function() {
+    this.raycaster.setFromCamera( this.mouse, this.camera );
+    var intersects = this.raycaster.intersectObjects( this.objs );
+    if (intersects.length > 0) {
+      if (this.selectedObj != intersects[0].object) {
+        if (this.selectedObj) {
+          this.selectedObj.material.color.set(0xffffff);
+        }
+        this.selectedObj = intersects[0].object
+        this.selectedObj.material.color.set(0x000000);
+      }
+    } else {
+      if (this.selectedObj) {
+        this.selectedObj.material.color.set(0xffffff);
+        this.selectedObj = null;
+      }
+    }
+};
