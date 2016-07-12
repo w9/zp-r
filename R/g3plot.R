18ae1968046abac2c6982261d45fa9a47a997786
Xun Zhu <zhuxun2@gmail.com>
2016-07-11 16:38:15 -1000

    commit

--- R/g3plot.R
+++ R/g3plot.R
@@ -6,13 +6,13 @@
 qplot3 <- function(mappings, data, verbose_level=0, show_datum=F) {
   # TODO: mappings sanity check
 
-  folder_name <- '3js01'
+  folder_name <- '3js'
   temp_dir <- tempdir()
   if (verbose_level > 1) message(sprintf('Creating directory %s', temp_dir))
 
   if (!file.exists(file.path(temp_dir, folder_name))) {
-    plotter_dir <- system.file('3js', package='ggplot3')
-    file.copy(plotter_dir, file.path(temp_dir, folder_name), recursive=T)
+    plotter_dir <- system.file(folder_name, package='ggplot3')
+    file.symlink(plotter_dir, file.path(temp_dir, folder_name))
     if (verbose_level > 1) message(sprintf('Copy %s to %s', plotter_dir, temp_dir))
   }
 
