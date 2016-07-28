9c4bf2ef37274a31aa8d7df1eb26425144905fe7
Xun Zhu <zhuxun2@gmail.com>
2016-07-27 17:38:32 -1000

    commit

--- R/data.R
+++ R/data.R
@@ -1,8 +1,14 @@
-#' Test Data set
-#'
-'ret'
-
 #' Glioblastoma Patient Dataset
 #'
 #' From Patel et al. 2014.
+#'
+#' @format Each row is a cell sample taken from one of the five patients.
 'patients'
+
+
+#' 3D tSNE of gene expression profile across cells from patient MGH30
+#'
+#' From Patel et al. 2014.
+#'
+#' @format Each row is a gene.
+'MGH30_genes'
