d6c340f1e8733479a36e18adae5310e0318943db
Xun Zhu <zhuxun2@gmail.com>
2016-08-03 08:49:02 -1000

    commit

--- R/zp.R
+++ R/zp.R
@@ -1,32 +1,15 @@
-# #' 3D scatter plot on Rstudio viewer
-# #'
-# #' @export
-# #' @examples
-# #' library(ggplot2)
-# #' data(ret)
-# #' qplot3(aes(x=x, y=y, z=z, color=group), data=ret)
-# qplot3 <- function(mapping, data, verbose_level=0) {
-#   # TODO: mapping sanity check
+# TODO: new API:
+#
+#                  zp(df) %>%
+#                    zp_coord(pc1, pc2, pc3) %>%
+#                    zp_coord(mds1, mds2, mds3) %>%
+#                    zp_color(group) %>%
+#                    zp_color(expr)
 # 
-#   folder_name <- 'js16'
-#   temp_dir <- tempdir()
-#   if (verbose_level > 1) message(sprintf('Creating directory %s', temp_dir))
-# 
-#   if (!file.exists(file.path(temp_dir, folder_name))) {
-#     plotter_dir <- system.file('zp', package='zp')
-#     file.copy(plotter_dir, temp_dir, recursive=T)
-#     file.rename(file.path(temp_dir, 'zp'), file.path(temp_dir, folder_name))
-#     if (verbose_level > 1) message(sprintf('Copy %s to %s', plotter_dir, temp_dir))
-#   }
-# 
-#   json_string <- jsonlite::toJSON(list(data=data, mapping=lapply(mapping, as.character)),
-#                                   dataframe='columns', auto_unbox=T)
-# 
-#   write(json_string, file.path(temp_dir, folder_name, 'query.json'));
-# 
-#   rstudioapi::viewer(paste0(file.path(temp_dir, folder_name, 'index.html')))
-# }
 
+
+#' Aesthetic object for ZP
+#'
 #' @import pryr
 #' @export
 zpa <- function(...) {
@@ -50,7 +33,7 @@ zpa <- function(...) {
 #'
 #' zp(MGH30genes, list(Correlation_tSNE=zpa(x=tsne1, y=tsne2, z=tsne3, color=pathway)))
 zp <-
-  function(data_, mappings_, use_viewer_=F) {
+  function(data_, mappings_, use_viewer_=T) {
     msg <- list()
     msg$data <- data_
     
