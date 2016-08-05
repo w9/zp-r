858558f972d32219ac87c7d2b2f47a5a0b4241e0
Xun Zhu <zhuxun2@gmail.com>
2016-08-04 14:29:43 -1000

    commit

--- R/zp.R
+++ R/zp.R
@@ -28,7 +28,7 @@ zpa <- function(...) {
 #'   zp_coord(tsne1, tsne2, tsne3) %>%
 #'   zp_color(pathway)
 zp <-
-  function(data_, use_viewer=T) {
+  function(data_, use_viewer=F) {
     x <- list()
     x$data <- data_
     x$mappings <- list()
